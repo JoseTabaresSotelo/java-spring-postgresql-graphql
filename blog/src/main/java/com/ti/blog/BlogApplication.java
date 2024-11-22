cbc8bd29d3f335431f13c4102eaad49d23d238ee
Jose Tabares Sotelo <jose.tabares.sotelo@accenture.com>
2024-11-22 00:51:04 -0600

    Initial commit using spring boot architecture

---
+++ blog/src/main/java/com/ti/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.ti.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+		System.out.println("Testing the class");
+	}
+
+}
